f5e6c9ea9a3ee980b74b98067f5b14659f249d39
Erick <erick.kuwahara79@gmail.com>
2025-04-27 13:04:08 -0300

    feat: implement controller, repository and mappers for category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
-import { Category } from './domain/category.entity';
+import { CreateCategoryController } from './http-server/create-category.controller';
+import { CreateCategoryUseCase } from './domain/use-cases/create-category.service';
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
+import { CategoriesRepository } from './domain/ports/categories.repository';
+import { PrismaCategoriesRepository } from './persistence/database/prisma/prisma-categories.repository';
 
 @Module({
-  providers: [Category],
+  controllers: [CreateCategoryController],
+  providers: [
+    CreateCategoryUseCase,
+    PrismaService,
+    {
+      provide: CategoriesRepository,
+      useClass: PrismaCategoriesRepository,
+    },
+  ],
 })
 export class CategoryModule {}
