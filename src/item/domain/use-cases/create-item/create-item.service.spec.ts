ccd07d02ce695d27698d185cf946c043dce930e9
IgorOGregorio <igorgregoriomg@gmail.com>
2025-05-02 17:23:44 -0300

    ref: update create item test

--- src/item/domain/use-cases/create-item/create-item.service.spec.ts
+++ src/item/domain/use-cases/create-item/create-item.service.spec.ts
@@ -1,14 +1,15 @@
 import { randomUUID } from 'node:crypto';
 import { Category } from '../../../../category/domain/category.entity';
-import { InMemoryCategoriesRepository } from '../../../../category/persistence/database/in-memory/in-memory-categories.repository';
-import { InMemoryItemsRepository } from '../../../persistence/in-memory/in-memory-items.repository';
-import { CreateItemProps } from '../../item.entity';
+import { CreateItemProps, Item } from '../../item.entity';
 import { CreateItemUseCase } from './create-item.service';
 import { UniqueEntityID } from '../../../../shared/entities/unique-entity-id';
+import { ItemsRepository } from '../../ports/items.repository';
+import { CategoriesRepository } from '../../../../category/domain/ports/categories.repository';
+import { Test } from '@nestjs/testing';
 
 describe('Create Item Use Case', () => {
-  let inMemoryItemsRepository: InMemoryItemsRepository;
-  let inMemoryCategoriesRepository: InMemoryCategoriesRepository;
+  let itemsRepository: ItemsRepository;
+  let categoriesRepository: CategoriesRepository;
   let sut: CreateItemUseCase;
 
   const createItemProps: CreateItemProps = {
@@ -21,45 +22,69 @@ describe('Create Item Use Case', () => {
     updatedAt: new Date(),
   };
 
-  beforeEach(() => {
-    inMemoryItemsRepository = new InMemoryItemsRepository();
-    inMemoryCategoriesRepository = new InMemoryCategoriesRepository();
-    sut = new CreateItemUseCase(
-      inMemoryItemsRepository,
-      inMemoryCategoriesRepository,
-    );
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        CreateItemUseCase,
+        {
+          provide: ItemsRepository,
+          useValue: {
+            create: jest.fn(),
+            findByName: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: CategoriesRepository,
+          useValue: {
+            findById: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    sut = moduleRef.get<CreateItemUseCase>(CreateItemUseCase);
+    itemsRepository = moduleRef.get<ItemsRepository>(ItemsRepository);
+    categoriesRepository =
+      moduleRef.get<CategoriesRepository>(CategoriesRepository);
+  });
+  it('should create an item', async () => {
     jest
-      .spyOn(inMemoryCategoriesRepository, 'findById')
+      .spyOn(categoriesRepository, 'findById')
       .mockResolvedValue(
         Category.create(
           { name: 'Test Category' },
           new UniqueEntityID(createItemProps.categoryId),
         ),
       );
-  });
-  it('should create an item', async () => {
     const item = await sut.execute(createItemProps);
     expect(item).toBeDefined();
-    expect(inMemoryItemsRepository.items).toHaveLength(1);
-    expect(inMemoryItemsRepository.items[0].name).toBe('Test Name');
     expect(item.categoryId).toBe(createItemProps.categoryId);
   });
   it('should create an item without image', async () => {
+    jest
+      .spyOn(categoriesRepository, 'findById')
+      .mockResolvedValue(
+        Category.create(
+          { name: 'Test Category' },
+          new UniqueEntityID(createItemProps.categoryId),
+        ),
+      );
     delete createItemProps.image;
     const item = await sut.execute(createItemProps);
     expect(item.image).toBe(null);
   });
   it('should throw an error if item with this name already exists', async () => {
-    await sut.execute(createItemProps);
+    jest
+      .spyOn(itemsRepository, 'findByName')
+      .mockResolvedValue(Item.create(createItemProps));
+
     await expect(() => sut.execute(createItemProps)).rejects.toThrow(
       new Error('Item with this name already exists'),
     );
   });
   it('should throw an error if category does not exist', async () => {
-    //mock category repository to return null
-    jest
-      .spyOn(inMemoryCategoriesRepository, 'findById')
-      .mockResolvedValue(null);
+    jest.spyOn(categoriesRepository, 'findById').mockResolvedValue(null);
 
     await expect(() => sut.execute(createItemProps)).rejects.toThrow(
       new Error('Invalid category'),
@@ -68,7 +93,7 @@ describe('Create Item Use Case', () => {
   it('should throw an error if category is deleted', async () => {
     //mock category repository to return deleted
     jest
-      .spyOn(inMemoryCategoriesRepository, 'findById')
+      .spyOn(categoriesRepository, 'findById')
       .mockResolvedValue(
         Category.create(
           { name: 'Test Category', deletedAt: new Date() },
